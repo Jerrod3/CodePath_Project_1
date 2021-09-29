55eb7de44d28fe44651668252ea20758222f655d
Jerrod C Lepper <lepperj@oregonstate.edu>
2021-09-29 19:44:57 -0400

    Pushing part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jerrod on 9/28/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
